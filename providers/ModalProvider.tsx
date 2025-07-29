dbf5714b9c990631a9727e5b8e83a66a4e4e7aa5
Ngoc Mai <mailyngoc2301@gmail.com>
2025-07-30 00:47:21 +0700

    feat: upload mp3 file & image

--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useEffect, useState } from "react";
 import AuthModal from "@/components/AuthModal";
+import UploadModal from "@/components/UploadModal";
 
 const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -12,6 +13,7 @@ const ModalProvider = () => {
   return (
     <>
       <AuthModal />
+      <UploadModal />
     </>
   );
 };
